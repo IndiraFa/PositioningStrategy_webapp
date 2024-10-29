a6c91c7d9ca004b933d5050fef691d63a67ecd3c
nnmp020395 <nnmp020395@gmail.com>
2024-10-29 14:21:30 +0100

    tag selection module : complete

--- src/tags_nutriscore_correlation.py
+++ src/tags_nutriscore_correlation.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import re
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+import re, os
 from pathlib import Path
 import argparse
+from functools import reduce
 
 # id == nom_recette sur streamlit 
 def get_value_from_string(string):
@@ -53,10 +54,9 @@ class tags:# mettre en majuscule Tags
         Output: data tags prétraité
         """
         listtags = self.tags
-        print(type(listtags))
         # Metter en miniscule les tags
         self.tags = list(map(lambda x: [tag.lower() for tag in x], listtags))
-        print(self.tags)
+
         # Enlever les tirets
         self.tags = list(map(lambda x: [tag.replace('-', ' ') for tag in x], listtags))
         return self.tags
@@ -75,7 +75,6 @@ class tags:# mettre en majuscule Tags
             for tagcase, id in zip(self.tags, self.ids):
                 if tag in tagcase: 
                     ids_target.append(id)
-        
         if len(ids_target) == 0:
             print("No recipe have this tag")
             return 0
@@ -85,34 +84,58 @@ class tags:# mettre en majuscule Tags
     def get_recipes_from_tags(self):
         """ Use to extract recipes with many tags """
         ids_target = []
-        if len(self.tag_target) > 1:
-            for tag in self.tag_target:
-                ids_target.append(self.extract_tag(tag))
-            # Union or intersection of recipes with tags
-            # list recipes have all tags target
-            # recipes1 = np.intersect1d(ids_target)
-            recipes1 = set(ids_target[0]).intersection(*ids_target[1:])
-            # list test have one tag target
-            # recipes2 = np.union1d(ids_target)
-            return ids_target, recipes1
-        else:
-            ids_target = self.extract_tag(self.tag_target)
-            return ids_target
+        tag_target = self.tag_target.split(',')
+        print(tag_target)
+       
+        if isinstance(tag_target, list):
+            if len(tag_target) == 1:
+                print("1", tag_target[0])
+                ids_target = self.extract_tag(tag_target[0])
+            else:
+                print("2")
+                for tag in tag_target:
+                    tmp = self.extract_tag(tag)
+                    ids_target.append(tmp)
+                    print('number of recipes for each tag', len(tmp))
+               
+                # Union or intersection of recipes with tags
+                # list recipes have all tags target
+                recipes1 = list(reduce(lambda i, j: i & j, (set(x) for x in ids_target)))
+                print('len of recipes1:',len(recipes1))
+                # list test have one tag target
+                # recipes2 = np.union1d(ids_target)
+                
+        elif isinstance(tag_target, str):
+            print("3")
+            ids_target = self.extract_tag(tag_target)
+        return ids_target
         
 
 def main(args):
     tags_reference = args.tags_reference
+    print(tags_reference, type(tags_reference))
+    
+    # create test directory to save test output 
+    dir_test = '/Users/phuongnguyen/Documents/cours_BGD_Telecom_Paris_2024/Kit_Big_Data/scripts/PositioningStrategy_webapp/tests/tags'
+    if not os.path.exists(dir_test):
+        os.makedirs(dir_test)
+
+    # load data
     path = Path('/Users/phuongnguyen/Documents/cours_BGD_Telecom_Paris_2024/Kit_Big_Data/dataset/RAW_recipes.csv')
     data = pd.read_csv(path)
     # dftags = data.tags.apply(get_text_from_string)
 
     #get list off all tags in the dataset
     tagsget, _ = tags(data, tags_reference).get_data_tags()
+    # print(tagsget[10])
 
     # get recipes with tags target
     tags_instance = tags(data, tags_reference)
     ids_recipes_target = tags_instance.get_recipes_from_tags()
     print('number of recipes with tags target:', len(ids_recipes_target))
+    for i in range(len(args.tags_reference.split(','))):
+        recipes_target = data.iloc[i]
+        recipes_target.to_csv(Path(dir_test,f'out_selected_tags{i+1}.csv'), index=False, header=True)
 
 
 
