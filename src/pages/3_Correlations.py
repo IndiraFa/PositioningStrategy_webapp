198660e91f1ff7697ae344d5ed6f67ebbf9c9a76
__TMSquared__ <44948364+TM-Squared@users.noreply.github.com>
2024-11-04 13:54:28 +0100

    Revert "Telecom paris/logs"

--- src/pages/3_Correlations.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Positioning strategy for Mangetamain")
-st.header("We explored for Mangetamain the opportuniy to position itself as a healthy food brand.") 
-st.text("We built a Nutrition Score based on daily value intaked recommandations.")
-
-st.subheader("Nutri-Score")
