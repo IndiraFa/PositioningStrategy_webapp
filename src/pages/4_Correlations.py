83cb923f1b25b37d087ca08ebebe1e5f88b06917
IndiraFa <fabre.indira@gmail.com>
2024-11-13 12:30:22 +0100

    update dataset path and formatting

--- src/pages/4_Correlations.py
+++ src/pages/4_Correlations.py
@@ -15,16 +15,40 @@ st.set_page_config(layout="centered")
 #allows to import packages from the parent folder
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-st.markdown("<h1 style='color:purple;'>Correlation analysis</h1>", unsafe_allow_html=True)
-#ajouter la conclusion de l'analyse de corrélation : pour améliorer la crédibilité du site, mettre en place un système de vérification des données nutritionnelles ou de calcul automatisé. 
-st.write("""
-## Correlation between characterics of recipes and their nutritional score
-""")
+st.markdown(
+    "<h1 style='color:purple;'>Correlation analysis</h1>",
+    unsafe_allow_html=True
+)
 
-columns_of_interest = ['dv_calories_%', 'dv_total_fat_%', 'dv_sugar_%', 'dv_sodium_%', 'dv_protein_%', 'dv_sat_fat_%', 'dv_carbs_%', 'nutriscore', 'minutes', 'n_steps', 'n_ingredients'] 
+# ajouter la conclusion de l'analyse de corrélation : pour améliorer la 
+# crédibilité du site, mettre en place un système de vérification des données 
+# nutritionnelles ou de calcul automatisé. 
+st.write(
+    """
+    ## Correlation between characterics of recipes and their nutritional score
+    """
+)
+
+columns_of_interest = [
+    'dv_calories_%',
+    'dv_total_fat_%',
+    'dv_sugar_%',
+    'dv_sodium_%',
+    'dv_protein_%',
+    'dv_sat_fat_%',
+    'dv_carbs_%',
+    'nutriscore',
+    'minutes',
+    'n_steps',
+    'n_ingredients'
+] 
 
 # Sélectionner les colonnes pour la matrice de corrélation
-selected_columns = st.multiselect("Select columns for correlation matrix", columns_of_interest, default=columns_of_interest)
+selected_columns = st.multiselect(
+    "Select columns for correlation matrix",
+    columns_of_interest,
+    default=columns_of_interest
+)
 
 # Calculer la matrice de corrélation pour les colonnes sélectionnées
 if selected_columns:
@@ -41,16 +65,30 @@ else:
     st.write("Please select at least one column.")
 
 
-st.write("""
-## Correlation between interactions and nutritional score of the recipes.
-         We computed for each recipe the number of interactions (reviews + ratings), number of reviews, number of ratings and the average rating, along with the Nutriscore.
-""")
+st.write(
+    """
+    ## Correlation between interactions and nutritional score of the recipes.
+         We computed for each recipe the number of interactions 
+         (reviews + ratings), number of reviews, number of ratings and the 
+         average rating, along with the Nutriscore.
+         """
+)
 
 st.dataframe(merged_data)
 
-columns_of_interest = ['interaction_count', 'review_count', 'rating_count', 'average_rating', 'nutriscore']
+columns_of_interest = [
+    'interaction_count',
+    'review_count',
+    'rating_count',
+    'average_rating',
+    'nutriscore'
+]
 
-selected_columns_2 = st.multiselect("Select columns for correlation matrix", columns_of_interest, default=columns_of_interest)
+selected_columns_2 = st.multiselect(
+    "Select columns for correlation matrix",
+    columns_of_interest,
+    default=columns_of_interest
+)
 
 if selected_columns_2:
     matrix = correlation_matrix(merged_data, selected_columns_2)
@@ -64,4 +102,12 @@ else:
 
 st.dataframe(label_analysis_result)
 
-st.write("D-labeled recipes have the best average rating (followed closely by C and B-labelled recipes). They also have the best interaction/recipe ratio, followed by E-labelled recipes. Although these varaitions are only by XXXXX% this is a sign that recipes published on the website should not be selected solely on their Nutriscore, because users also enjoy recipes with a lower Nutriscore.")
+st.write(
+    """D-labeled recipes have the best average rating (followed closely by 
+    C and B-labelled recipes). They also have the best interaction/recipe 
+    ratio, followed by E-labelled recipes. Although these varaitions are only 
+    by XXXXX% this is a sign that recipes published on the website should 
+    not be selected solely on their Nutriscore, because users also enjoy 
+    recipes with a lower Nutriscore.
+    """
+)
