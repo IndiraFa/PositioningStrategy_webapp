57b6f3eb294ef8c81d0b159403b40d7ffe74bd29
IndiraFa <fabre.indira@gmail.com>
2024-11-13 14:29:03 +0100

    datasets path modifs and formatting

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,10 +1,24 @@
 import pandas as pd
 import re
-#from tabulate import tabulate
+# from tabulate import tabulate
 
 configs = {
-        'nutritioncolname': ['calories', 'total_fat_%', 'sugar_%', 'sodium_%', 'protein_%', 'sat_fat_%', 'carbs_%'],
-        'grillecolname': ['dv_calories_%', 'dv_sat_fat_%', 'dv_sugar_%', 'dv_sodium_%', 'dv_protein_%'],
+        'nutritioncolname': [
+            'calories',
+            'total_fat_%',
+            'sugar_%',
+            'sodium_%',
+            'protein_%',
+            'sat_fat_%',
+            'carbs_%'
+        ],
+        'grillecolname': [
+            'dv_calories_%',
+            'dv_sat_fat_%',
+            'dv_sugar_%',
+            'dv_sodium_%',
+            'dv_protein_%'
+        ],
         'dv_calories': 2000
 }
 
@@ -38,7 +52,8 @@ class Preprocessing:
         self.normaldata = self.set_dv_normalisation()
         self.prefiltredata = self.prefiltrage()
         self.gaussiandata, self.outliers = self.gaussian_normalisation()
-        self.denormalizedata, self.denormalized_outliers = self.Denormalisation(self.gaussiandata, self.outliers)
+        self.denormalizedata, self.denormalized_outliers = \
+        self.Denormalisation(self.gaussiandata, self.outliers)
 
     def get_raw_nutrition(self):
         try:
@@ -53,9 +68,15 @@ class Preprocessing:
         try:
             # Format the nutrition data by extracting numerical values
             data = self.get_raw_nutrition()
-            formatted_data = data['nutrition'].apply(lambda x: Datatools.get_value_from_string(x))
-            # Create a DataFrame with the formatted data and add the 'id' column
-            table = pd.DataFrame(formatted_data.tolist(), columns=self.configs['nutritioncolname'])
+            formatted_data = (
+                data['nutrition']
+                .apply(lambda x: Datatools.get_value_from_string(x))
+            )
+            # Create a df with the formatted data and add the 'id' column
+            table = pd.DataFrame(
+                formatted_data.tolist(),
+                columns=self.configs['nutritioncolname']
+            )
             table['id'] = data['id'].values
             return table
         except KeyError as e:
@@ -75,9 +96,14 @@ class Preprocessing:
             table = pd.DataFrame()
             table['id'] = fttable['id']
             # Calculate the percentage of daily values for each nutrient
-            table['dv_calories_%'] = (fttable['calories'] * 100 / dv_calories).round(2)
+            table['dv_calories_%'] = (
+                fttable['calories'] * 100 / dv_calories
+                ).round(2)
             for col in self.configs['nutritioncolname'][1:]:
-                table[f'dv_{col}'] = (fttable[col] * dv_calories / fttable['calories']).round(2)
+                table[f'dv_{col}'] = (
+                    (fttable[col] * dv_calories / fttable['calories'])
+                    .round(2)
+                )
             self.normaldata = table
             return table
         except KeyError as e:
@@ -98,29 +124,68 @@ class Preprocessing:
             table_prefiltre = self.normaldata.copy()
 
             # Identify outliers for each column based on predefined thresholds
-            outliers_calories = table_prefiltre[table_prefiltre['dv_calories_%'] > 5000]
-            outliers_total_fat = table_prefiltre[table_prefiltre['dv_total_fat_%'] > 5000]
-            outliers_sugar = table_prefiltre[table_prefiltre['dv_sugar_%'] > 50000]
-            outliers_sodium = table_prefiltre[table_prefiltre['dv_sodium_%'] > 5000]
-            outliers_protein = table_prefiltre[table_prefiltre['dv_protein_%'] > 2000]
-            outliers_sat_fat = table_prefiltre[table_prefiltre['dv_sat_fat_%'] > 2000]
-            outliers_carbs = table_prefiltre[table_prefiltre['dv_carbs_%'] > 5000]
+            outliers_calories = table_prefiltre[
+                table_prefiltre['dv_calories_%'] > 5000
+            ]
+            outliers_total_fat = table_prefiltre[
+                table_prefiltre['dv_total_fat_%'] > 5000
+            ]
+            outliers_sugar = table_prefiltre[
+                table_prefiltre['dv_sugar_%'] > 50000
+            ]
+            outliers_sodium = table_prefiltre[
+                table_prefiltre['dv_sodium_%'] > 5000
+            ]
+            outliers_protein = table_prefiltre[
+                table_prefiltre['dv_protein_%'] > 2000
+            ]
+            outliers_sat_fat = table_prefiltre[
+                table_prefiltre['dv_sat_fat_%'] > 2000
+            ]
+            outliers_carbs = table_prefiltre[
+                table_prefiltre['dv_carbs_%'] > 5000
+            ]
 
             # Combine all outliers into a single DataFrame
-            outliers = pd.concat([outliers_calories, outliers_total_fat, outliers_sugar, outliers_sodium, outliers_protein, outliers_sat_fat, outliers_carbs]).drop_duplicates()
+            outliers = pd.concat([
+                outliers_calories,
+                outliers_total_fat,
+                outliers_sugar,
+                outliers_sodium,
+                outliers_protein, 
+                outliers_sat_fat,
+                outliers_carbs
+            ]).drop_duplicates()
 
             # Filter out the visible outliers from different columns
-            table_prefiltre = table_prefiltre[table_prefiltre['dv_calories_%'] <= 5000]
-            table_prefiltre = table_prefiltre[table_prefiltre['dv_total_fat_%'] <= 5000]
-            table_prefiltre = table_prefiltre[table_prefiltre['dv_sugar_%'] <= 50000]
-            table_prefiltre = table_prefiltre[table_prefiltre['dv_sodium_%'] <= 5000]
-            table_prefiltre = table_prefiltre[table_prefiltre['dv_protein_%'] <= 2000]
-            table_prefiltre = table_prefiltre[table_prefiltre['dv_sat_fat_%'] <= 2000]
-            table_prefiltre = table_prefiltre[table_prefiltre['dv_carbs_%'] <= 5000]
+            table_prefiltre = table_prefiltre[
+                table_prefiltre['dv_calories_%'] <= 5000
+            ]
+            table_prefiltre = table_prefiltre[
+                table_prefiltre['dv_total_fat_%'] <= 5000
+            ]
+            table_prefiltre = table_prefiltre[
+                table_prefiltre['dv_sugar_%'] <= 50000
+            ]
+            table_prefiltre = table_prefiltre[
+                table_prefiltre['dv_sodium_%'] <= 5000
+            ]
+            table_prefiltre = table_prefiltre[
+                table_prefiltre['dv_protein_%'] <= 2000
+            ]
+            table_prefiltre = table_prefiltre[
+                table_prefiltre['dv_sat_fat_%'] <= 2000
+            ]
+            table_prefiltre = table_prefiltre[
+                table_prefiltre['dv_carbs_%'] <= 5000
+            ]
 
             final_count = len(table_prefiltre)
             print(f"Number of rows after pre-filtering: {final_count}")
-            print(f"Number of outliers removed during pre-filtering: {initial_count - final_count}")
+            print(
+                f"Number of outliers removed during pre-filtering: "
+                f"{initial_count - final_count}"
+            )
             print("\n")
 
             # Store the outliers
@@ -137,12 +202,21 @@ class Preprocessing:
             return pd.DataFrame()
 
     def gaussian_normalisation(self):
-        gauss_configs = {'colname': ['dv_calories_%', 'dv_total_fat_%', 'dv_sugar_%', 'dv_sodium_%', 'dv_protein_%', 'dv_sat_fat_%', 'dv_carbs_%']}
+        gauss_configs = {'colname': [
+            'dv_calories_%',
+            'dv_total_fat_%',
+            'dv_sugar_%',
+            'dv_sodium_%',
+            'dv_protein_%',
+            'dv_sat_fat_%',
+            'dv_carbs_%'
+        ]}
         try:
             table_gauss = self.prefiltredata.copy()
             
             initial_outliers_count = len(self.outliers)
-            print(f"Number of rows in the outliers DataFrame before processing: {initial_outliers_count}") #test unitaire
+            print(f"Number of rows in the outliers DataFrame before processing: "
+                  f"{initial_outliers_count}") #test unitaire
 
             # Apply Gaussian normalization to each column
             for col in gauss_configs['colname']:
@@ -160,8 +234,12 @@ class Preprocessing:
             DF_noOutliers = table_gauss.copy()
             DF_outliers = self.outliers.copy()
 
-            print(f"Size of DF_noOutliers before processing: {len(DF_noOutliers)}") #test unitaire
-            print(f"Size of Total_outliers before processing: {len(DF_outliers)}") #test unitaire
+            print(
+                f"Size of DF_noOutliers before processing: "
+                f"{len(DF_noOutliers)}") #test unitaire
+            print(
+                f"Size of Total_outliers before processing: "
+                f"{len(DF_outliers)}") #test unitaire
             print("\n")
 
             # Identify and remove outliers (values >= 3) for each column
@@ -169,7 +247,9 @@ class Preprocessing:
                 if col == 'id':
                     continue
                 col_outliers = DF_noOutliers[DF_noOutliers[col] >= 3]
-                print(f"Number of outliers found in column {col}: {len(col_outliers)}")
+                print(
+                    f"Number of outliers found in column {col}: "
+                    f"{len(col_outliers)}")
                 DF_outliers = pd.concat([DF_outliers, col_outliers])
                 DF_noOutliers = DF_noOutliers[DF_noOutliers[col] < 3].copy()
 
@@ -177,8 +257,10 @@ class Preprocessing:
             self.outliers = DF_outliers.drop_duplicates()
 
             print("\n")
-            print(f"Size of DF_noOutliers after processing: {len(DF_noOutliers)}") #test unitaire
-            print(f"Size of DF_outliers after processing: {len(DF_outliers)}") 
+            print(
+                f"Size of DF_noOutliers after processing: "
+                f"{len(DF_noOutliers)}") #test unitaire
+            print(f"Size of DF_outliers after processing: {len(DF_outliers)}")
             print("\n")
 
             return DF_noOutliers, DF_outliers
@@ -190,15 +272,20 @@ class Preprocessing:
             # Print an unexpected error message
             print(f"Unexpected error: {e}")
             return pd.DataFrame()
-        
-    def Denormalisation(self, DF_noOutliers, DF_outliers): # denormalisation of the data from the gaussian_normalisation
+
+   # denormalisation of the data from the gaussian_normalisation     
+    def Denormalisation(self, DF_noOutliers, DF_outliers): 
         try:
             # Denormalize the Gaussian normalized data
             finalDF_noOutliers = DF_noOutliers.copy()
             finalDF_outliers = DF_outliers.copy()
             
-            # Combine the columns from grillecolname with dv_total_fat_% and dv_carbs_%
-            columns_to_denormalize = self.configs['grillecolname'] + ['dv_total_fat_%', 'dv_carbs_%']
+            # Combine the columns from grillecolname with 
+            # dv_total_fat_% and dv_carbs_%
+            columns_to_denormalize = (
+                self.configs['grillecolname'] \
+                + ['dv_total_fat_%', 'dv_carbs_%']
+            )
             
             for col in columns_to_denormalize:
                 mu = self.mu_values[col]
@@ -217,7 +304,7 @@ class Preprocessing:
             return pd.DataFrame()
 
 def main():
-    path = '/Users/fabreindira/Library/CloudStorage/OneDrive-telecom-paristech.fr/MS_BGD/KitBigData/Projet_kitbigdata/data_base/RAW_recipes.csv'
+    path = './datasets/RAW_recipes.csv'
 
     # Create an instance of the Preprocessing class
     preprocessing_instance = Preprocessing(path, configs)
